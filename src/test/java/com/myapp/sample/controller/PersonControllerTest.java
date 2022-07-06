e53f37c5cb8f6a034edc12ab3322c5100803e191
apgandhamwar <apgandhamwar@gmail.com>
2022-07-06 19:36:40 +0530

    Update PersonControllerTest.java

--- src/test/java/com/myapp/sample/controller/PersonControllerTest.java
+++ src/test/java/com/myapp/sample/controller/PersonControllerTest.java
@@ -42,29 +42,50 @@ public class PersonControllerTest {
   
   @Test
   public void testRegister() throws Exception {
-    
-    Assert.assertEquals("test 1", "test 1");
-    Assert.assertEquals(200,200);
+    HttpEntity<Object> person = getHttpEntity(
+        "{\"name\": \"test 1\", \"email\": \"test10000000000001@gmail.com\"}");
+    ResponseEntity<Person> response = template.postForEntity(
+        "/api/person", person, Person.class);
+    Assert.assertEquals("test 1", response.getBody().getName());
+    Assert.assertEquals(200,response.getStatusCode().value());
   }
 
   @Test
   public void testGetAllPersons() throws Exception {
-    
-    Assert.assertEquals("test 1", "test 1");
-    Assert.assertEquals(200,200);
+    HttpEntity<Object> person = getHttpEntity(
+            "{\"name\": \"test 1\", \"email\": \"test10000000000001@gmail.com\"}");
+    ResponseEntity<Person> response = template.postForEntity(
+            "/api/person", person, Person.class);
+
+    ParameterizedTypeReference<List<Person>> responseType = new ParameterizedTypeReference<List<Person>>(){};
+    ResponseEntity<List<Person>> response2 = template.exchange("/api/person", HttpMethod.GET, null, responseType);
+    Assert.assertEquals(response2.getBody().size(), 1);
+    Assert.assertEquals("test 1", response2.getBody().get(0).getName());
+    Assert.assertEquals(200,response2.getStatusCode().value());
   }
 
   @Test
   public void testGetPersonById() throws Exception {
+    HttpEntity<Object> person = getHttpEntity(
+            "{\"name\": \"test 1\", \"email\": \"test10000000000001@gmail.com\"}");
+    ResponseEntity<Person> response = template.postForEntity(
+            "/api/person", person, Person.class);
 
-    Assert.assertEquals("test 1", "test 1");
-    Assert.assertEquals(200,200);
+    ParameterizedTypeReference<List<Person>> responseType = new ParameterizedTypeReference<List<Person>>(){};
+    ResponseEntity<List<Person>> response2 = template.exchange("/api/person", HttpMethod.GET, null, responseType);
+
+    Long id = response2.getBody().get(0).getId();
+    ResponseEntity<Person> response3 = template.getForEntity(
+            "/api/person/" + id, Person.class);
+    Assert.assertEquals("test 1", response3.getBody().getName());
+    Assert.assertEquals(200,response3.getStatusCode().value());
   }
 
   @Test
   public void testGetPersonByNull() throws Exception {
-    
-    Assert.assertEquals(404,404);
+    ResponseEntity<Person> response3 = template.getForEntity(
+            "/api/person/1", Person.class);
+    Assert.assertEquals(404,response3.getStatusCode().value());
   }
 
   private HttpEntity<Object> getHttpEntity(Object body) {
