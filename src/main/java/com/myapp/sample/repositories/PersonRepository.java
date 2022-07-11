5c25480e50e803fb6452c375c0fea4e6d14ddcde
apgandhamwar <apgandhamwar@gmail.com>
2022-07-11 13:40:58 +0530

    Update PersonRepository.java

--- src/main/java/com/myapp/sample/repositories/PersonRepository.java
+++ src/main/java/com/myapp/sample/repositories/PersonRepository.java
@@ -1,5 +1,7 @@
 package com.myapp.sample.repositories;
 
+
+
 import com.myapp.sample.model.Person;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
